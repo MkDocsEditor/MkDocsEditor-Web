c481d50c0e8a1b8319c06b1e8548d3baacad2d77
Markus Ressel <mail@markusressel.de>
2019-07-11 05:12:11 +0200

    added page titles use on-demand import for all pages

--- src/router.ts
+++ src/router.ts
@@ -6,22 +6,54 @@ import Settings from "@/components/Settings.vue";
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
     routes: [
-        {name: "Home", path: "/", component: FileBrowser},
-        {name: "FileBrowserMain", path: "/file_browser", component: FileBrowser},
-        {name: "FileBrowser", path: "/file_browser/:id", component: FileBrowser},
-        {name: "CodeEditor", path: "/code_editor/:id", component: CodeEditor},
-        {name: "Settings", path: "/settings", component: Settings},
         {
-            name: "AboutApp",
-            path: "/about",
+            name: "Home", path: "/", component: FileBrowser,
+            meta: {
+                title: "MkDocsEditor",
+                metaTags: [
+                    {name: "description", content: "The home page of MkDocsEditor.",},
+                    {property: "og:description", content: "The home page of MkDocsEditor.",},
+                ],
+            },
+        },
+        {
+            name: "FileBrowserMain", path: "/file_browser",
+            component: () => import(/* webpackChunkName: "codeEditor" */ "@/components/browser/FileBrowser.vue"),
+            meta: {title: "File Browser"},
+        },
+        {
+            name: "FileBrowser", path: "/file_browser/:id",
+            component: () => import(/* webpackChunkName: "codeEditor" */ "@/components/browser/FileBrowser.vue"),
+            meta: {title: "File Browser"},
+        },
+        {
+            name: "CodeEditor", path: "/code_editor/:id",
+            component: () => import(/* webpackChunkName: "codeEditor" */ "@/components/CodeEditor.vue"),
+            meta: {title: "Code Editor"},
+        },
+        {
+            name: "Settings", path: "/settings",
+            component: () => import(/* webpackChunkName: "settings" */ "@/components/Settings.vue"),
+            meta: {title: "Settings"},
+        },
+        {
+            name: "AboutApp", path: "/about",
             component: () => import(/* webpackChunkName: "about" */ "@/components/about/About.vue"),
+            meta: {title: "About"},
         },
         {
             name: "AboutLibraries",
             path: "/about/libraries",
             component: () => import(/* webpackChunkName: "about" */ "@/components/about/About.vue"),
+            meta: {title: "Libraries"},
         },
     ],
 });
+router.beforeEach((to, from, next) => {
+    document.title = to.meta.title;
+    next();
+});
+
+export default router;
